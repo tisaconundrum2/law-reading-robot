9db5a2a33373160da310ea16a698b78cce679a71
Nicholas Finch <ngf4@nau.edu>
2026-03-08 19:11:46 -0400

    Clean out everything

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { FilterProvider, FilterContainer } from "./Filter";
-export { SearchProvider, Search, useSearch } from "./Search";
-export { default as Results } from "./Results/Results";
-export { default as Footer } from "./Footer/Footer";
-export { default as Pagination } from "./Pagination/Pagination";
--- src/models/Bill/index.js
+++
@@ -1,3 +0,0 @@
-export { default as BillModel } from "./BillModel";
-export { default as BillFilter } from "./BillFilter";
-export * as Types from "./types";
